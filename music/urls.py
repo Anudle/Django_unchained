680095bf3c4510c7c159494367c927a0a8ae4adc
Anudle <AnuShrestha007@gmail.com>
2016-10-25 22:37:29 -0600

    404 and heart breaks

--- music/urls.py
+++ music/urls.py
@@ -2,5 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+#/music/
 url(r'^$', views.index, name='index'),
+
+#/music/69/
+url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 ]
+ 
--- music/views.py
+++ music/views.py
@@ -1,4 +1,15 @@
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
+from .models import Album
+
 
 def index(request):
-    return HttpResponse('<h1> This is the music app homepage </h1>')
+    all_albums = Album.objects.all()
+    return render(request, 'music/index.html', {'all_albums': all_albums})
+
+def detail(request, album_id):
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404('Album does not exsit')
+    return render(request, 'music/detail.html', {'album': album})
